d8ec7b577f6172aa524b87813428aac709b77b5d
Kai Washizaki <washizaki@fusic.co.jp>
2020-04-04 18:29:32 +0900

    loggingに関する設定を追加

--- config.py
+++ config.py
@@ -1,8 +1,43 @@
+# coding:utf-8
+
+"""
+設定パラメータをConfigerクラスにまとめる
+
+loggingの設定
+ - 'PYTHON_LOG_LEVEL' に DEBUG, INFO, ERRORなどを設定
+"""
+
 import os
 import logging
+import logging.handlers
+
+###############
+# loggingの設定 #
+###############
+
+_LOG_LEVEL_ENV = 'PYTHON_LOG_LEVEL'
+_LOG_FORMAT = '%(asctime)s | %(levelname)-8s | %(filename)-12s - %(funcName)-12s : %(lineno)-4s | %(message)s'
+_LOG_FORMAT = logging.Formatter(_LOG_FORMAT)
+
+_sh = logging.StreamHandler()
+_sh.setFormatter(_LOG_FORMAT)
+logging.getLogger().addHandler(_sh)
+
+_LOG_LEVEL = os.environ.get(_LOG_LEVEL_ENV)
+if _LOG_LEVEL == "DEBUG":
+    logging.getLogger().setLevel(logging.DEBUG)
+elif _LOG_LEVEL == "INFO":
+    logging.getLogger().setLevel(logging.INFO)
+elif _LOG_LEVEL == "ERROR":
+    logging.getLogger().setLevel(logging.ERROR)
+else:
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.info(f"{_LOG_LEVEL_ENV}が設定されていないのでDEBUGとして設定する")
+    _LOG_LEVEL = "DEBUG"
 
 
 class Configure(object):
-    log_level = os.environ.get('PYTHON_LOG_LEVEL')
-    if not log_level:
-        log_level = logging.DEBUG
+    LOG_LEVEL = _LOG_LEVEL
+
+
+logging.info("環境変数による設定が終了しました")
